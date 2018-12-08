636ac5717d89b7afa786c4b6b78cb1c6714f308f
novakand <45287302+novakand@users.noreply.github.com>
2018-12-08 22:29:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 
-
+/**
+ * @fileoverview 此类表示地图上的一个覆盖物，该覆盖物由文字和图标组成，从Overlay继承。
+ * 主入口类是<a href="symbols/BMapLib.Infobox.html">TextIconOverlay</a>，
+ * 基于Baidu Map API 1.2。
+ *
+ * @author Baidu Map Api Group 
+ * @version 1.2
+ * http://api.map.baidu.com/library/InfoBox/1.2/src/InfoBox.js
+ */
+ 
 (function (root, factory) {
     if (typeof exports === 'object') {
         module.exports = { default: factory(), InfoBox: factory() };
